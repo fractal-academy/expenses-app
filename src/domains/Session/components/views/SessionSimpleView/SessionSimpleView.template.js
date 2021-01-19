b6b249db9805a9b4a1a3e9259bfbc8ce1f7f6230
5P4C3GR33D <cyber.cuttlefish@gmail.com>
2021-01-19 12:41:54 +0200

    added session simple view component template; changed .gitignore file

--- src/domains/Session/components/views/SessionSimpleView/SessionSimpleView.template.js
+++ src/domains/Session/components/views/SessionSimpleView/SessionSimpleView.template.js
@@ -1,5 +1,35 @@
+import React from 'react'
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  root: {
+    textAlign: 'center'
+  }
+})
+
 const SessionSimpleView = (props) => {
-  return <div>SessionSimpleView</div>
+  const classes = useStyles(props)
+  return (
+    <Container>
+      <Box display="flex" justifyContent="center" mt={2}>
+        <Typography variant="h4">Sign in</Typography>
+      </Box>
+      <Box display="flex" p={1} className={classes.root}>
+        <Typography variant="subtitle2">
+          Use your company Google account @senseteq.io to log in.
+        </Typography>
+      </Box>
+      <Box display="flex" justifyContent="center" m={4}>
+        <Button variant="outlined" color="primary">
+          Sign in with Google
+        </Button>
+      </Box>
+    </Container>
+  )
 }
 
 SessionSimpleView.propTypes = {}
