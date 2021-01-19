c2ff9710ffbbceef50481de11e97871758a11bb7
Maxim <maxim.makarov14@gmail.com>
2021-01-19 18:07:28 +0200

    add currency select component

--- src/app/domains/Currency/components/select/CurrencySingleSelect/CurrencySingleSelect.template.js
+++ src/app/domains/Currency/components/select/CurrencySingleSelect/CurrencySingleSelect.template.js
@@ -1,8 +1,42 @@
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
+
+import { currency } from 'app/constants'
+
+const { CURRENCY, CURRENCY_KEYS } = currency
+
 const CurrencySingleSelect = (props) => {
-  return <div>CurrencySingleSelect</div>
+  const { itemProps } = props
+  const [currencySign, setCurrencySign] = useState(CURRENCY.USD.name)
+
+  const handleSelect = (event) => {
+    setCurrencySign(event.target.value)
+  }
+  return (
+    <Select
+      value={currencySign}
+      defaultValue={currencySign}
+      onChange={handleSelect}>
+      {CURRENCY_KEYS.map((item) => (
+        <MenuItem {...itemProps} value={CURRENCY[item].name}>
+          {CURRENCY[item].sign}
+        </MenuItem>
+      ))}
+    </Select>
+  )
 }
 
-CurrencySingleSelect.propTypes = {}
-CurrencySingleSelect.defaultProps = {}
+CurrencySingleSelect.propTypes = {
+  itemProps: PropTypes.shape({
+    dense: PropTypes.bool
+  })
+}
+CurrencySingleSelect.defaultProps = {
+  itemProps: {
+    dense: true
+  }
+}
 
 export default CurrencySingleSelect
