01e05db43ed7dea871c9770613cbdd04afdcc820
sashka2131 <uhucbeucg@gmail.com@gmail.com>
2021-01-25 12:45:23 +0200

    Add custom modal

---
+++ src/app/components/Lib/Modal/Modal.stories.js
@@ -0,0 +1,13 @@
+import Modal from './Modal.template'
+
+const metadata = {
+  title: 'domains/Measure/combined/Modal',
+  component: Modal
+}
+export default metadata
+
+const Template = (args) => <Modal {...args} />
+
+export const ModalStory = Template.bind({})
+
+Modal.args = {}
