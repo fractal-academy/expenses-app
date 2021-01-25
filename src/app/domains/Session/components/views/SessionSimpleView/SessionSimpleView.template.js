f078d1da6092528254a15f8c628966441f93fbb5
Rostyslav <rostyslavmarchuk440@gmail.com>
2021-01-25 16:24:00 +0200

    Changed bootstrap grid to qonsoll grid

--- src/app/domains/Session/components/views/SessionSimpleView/SessionSimpleView.template.js
+++ src/app/domains/Session/components/views/SessionSimpleView/SessionSimpleView.template.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import Box from '@material-ui/core/Box'
+import { Row, Col, Container } from '@qonsoll/react-design'
 import Button from '@material-ui/core/Button'
-import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import GTranslateOutlinedIcon from '@material-ui/icons/GTranslateOutlined'
@@ -16,26 +15,28 @@ const SessionSimpleView = (props) => {
   const classes = useStyles(props)
   return (
     <Container>
-      <Box display="flex" justifyContent="center">
-        <Typography variant="h4">Sign in</Typography>
-      </Box>
-      <Box
-        display="flex"
-        p={1}
-        justifyContent="center"
-        className={classes.root}>
-        <Typography variant="subtitle2">
-          Use your company Google account @senseteq.io to log in.
-        </Typography>
-      </Box>
-      <Box display="flex" justifyContent="center" m={4}>
-        <Button variant="outlined" color="primary">
-          <Box display="flex" justifyContent="center" pr={1}>
-            <GTranslateOutlinedIcon />
-          </Box>
-          Sign in with Google
-        </Button>
-      </Box>
+      <Row h="center">
+        <Row h="center" mb={1}>
+          <Col cw={[12]}>
+            <Typography variant="h4">Sign in</Typography>
+          </Col>
+        </Row>
+        <Row h="center">
+          <Col cw={[11]}>
+            <Typography variant="subtitle2" className={classes.root}>
+              Use your company Google account @senseteq.io to log in.
+            </Typography>
+          </Col>
+        </Row>
+        <Row h="center" my={4}>
+          <Col cw={[12]}>
+            <Button variant="outlined" color="primary">
+              <GTranslateOutlinedIcon />
+              Sign in with Google
+            </Button>
+          </Col>
+        </Row>
+      </Row>
     </Container>
   )
 }
