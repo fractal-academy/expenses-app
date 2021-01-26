c9c88e8695f159658f8b9f1a8c1068a8baf09869
5P4C3GR33D <cyber.cuttlefish@gmail.com>
2021-01-26 11:13:17 +0200

    fixed toolbar layout

--- src/app/components/Lib/Toolbar/Toolbar.template.js
+++ src/app/components/Lib/Toolbar/Toolbar.template.js
@@ -2,35 +2,41 @@ import PropTypes from 'prop-types'
 import CheckIcon from '@material-ui/icons/Check'
 import DeleteIcon from '@material-ui/icons/Delete'
 import { Container, Row, Col } from '@qonsoll/react-design'
-import {
-  Box,
-  Button,
-  Toolbar,
-  IconButton,
-  Typography
-} from '@material-ui/core/'
+import { Button, Toolbar, IconButton, Typography } from '@material-ui/core/'
 
 const Tolbar = (props) => {
   return (
     <Container display="box">
-      <Row h={props.numberChecked <= 0 && 'center'}>
-        <Col cw="auto">
+      <Row>
+        <Col>
           <Toolbar>
             {props.numberChecked > 0 ? (
-              <Box display="flex" justifyContent="space-between">
-                <Typography>{props.numberChecked + ' selected'}</Typography>
-                <IconButton aria-label="check" color="primary">
-                  <CheckIcon />
-                </IconButton>
-                <IconButton aria-label="delete" color="primary">
-                  <DeleteIcon />
-                </IconButton>
-              </Box>
+              <Container display="box">
+                <Row h="right" v="center">
+                  <Col>
+                    <Typography variant="button">
+                      {props.numberChecked + ' selected'}
+                    </Typography>
+                  </Col>
+                  <Col cw="auto">
+                    <IconButton aria-label="check" color="primary">
+                      <CheckIcon />
+                    </IconButton>
+                    <IconButton aria-label="delete" color="primary">
+                      <DeleteIcon />
+                    </IconButton>
+                  </Col>
+                </Row>
+              </Container>
             ) : (
-              <Box>
-                <Button color="primary">Wishes</Button>
-                <Button color="primary">Cart</Button>
-              </Box>
+              <Container display="box">
+                <Row h="center">
+                  <Col cw="auto">
+                    <Button color="primary">Wishes</Button>
+                    <Button color="primary">Cart</Button>
+                  </Col>
+                </Row>
+              </Container>
             )}
           </Toolbar>
         </Col>
