fda19fbd6b9d3cd6e27d1699a8f5cc109b88a355
okrdima <okrdima@gmail.com>
2021-01-27 11:10:25 +0200

    remove console.log()

--- src/app/domains/Priority/components/select/PrioritySingleSelect/PrioritySingleSelect.template.js
+++ src/app/domains/Priority/components/select/PrioritySingleSelect/PrioritySingleSelect.template.js
@@ -4,7 +4,6 @@ import MenuItem from '@material-ui/core/MenuItem'
 import PropTypes from 'prop-types'
 
 const PrioritySingleSelect = (props) => {
-  console.log(PRIORITY)
   return (
     <Select data={PRIORITY} value={PRIORITY[0]} fullWidth>
       {(item) => (
