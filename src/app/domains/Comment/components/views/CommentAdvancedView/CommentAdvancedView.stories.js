3a6bb0a007d00a945c6bc1f16cd9efd2f87dbbb6
aleksandrgolyk <aleksandr.golyk@gmail.com>
2021-01-28 17:05:35 +0200

    pulled config

--- src/app/domains/Comment/components/views/CommentAdvancedView/CommentAdvancedView.stories.js
+++ src/app/domains/Comment/components/views/CommentAdvancedView/CommentAdvancedView.stories.js
@@ -1,4 +1,5 @@
 import CommentAdvancedView from './CommentAdvancedView.template'
+import { ThemeProvider } from '@qonsoll/react-design'
 
 const metadata = {
   title: 'domains/Comment/components/views/CommentAdvancedView',
@@ -6,7 +7,16 @@ const metadata = {
 }
 export default metadata
 
-const Template = (args) => <CommentAdvancedView {...args} />
+const Template = (args) => (
+  <ThemeProvider theme={Theme}>
+    <CommentAdvancedView
+      {...args}
+      valueForProgressBar={150}
+      colorCategory="orange"
+      nameCategory="Office"
+    />
+  </ThemeProvider>
+)
 
 export const CommentAdvancedViewStory = Template.bind({})
 
--- src/app/domains/Comment/components/views/CommentAdvancedView/CommentAdvancedView.template.js
+++ src/app/domains/Comment/components/views/CommentAdvancedView/CommentAdvancedView.template.js
@@ -1,5 +1,27 @@
+import { useStyles } from './CommentAdvancedView.style'
+import { Paper } from '@material-ui/core'
+import { Box, Row, Col } from '@qonsoll/react-design'
+
 const CommentAdvancedView = (props) => {
-  return <div>CommentAdvancedView</div>
+  const classes = useStyles()
+
+  return (
+    <Box>
+      <Row>
+        <Col>
+          <Box m={2}>
+            <Paper elevation={3} square={true}>
+              qweqweqweqweqw qweqweqwe qweqweq Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Blanditiis dolore doloremque,
+              doloribus ea eaque earum eveniet in iste iusto laudantium
+              necessitatibus nesciunt officiis quasi repellendus rerum soluta,
+              tempore ullam vel?
+            </Paper>
+          </Box>
+        </Col>
+      </Row>
+    </Box>
+  )
 }
 
 CommentAdvancedView.propTypes = {}
