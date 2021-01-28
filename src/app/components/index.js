25b5cb94ef484ab970ee97ad45421bc43eadd7dd
Maxim <maxim.makarov14@gmail.com>
2021-01-28 22:11:21 +0200

    add plug for desktop screen

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -2,5 +2,6 @@ import { Header } from './Header'
 import { Navbar } from './Navbar'
 import { Settings } from './Settings'
 import { Layout } from './Layout'
+import { PlugForDesktop } from './PlugForDesktop'
 
-export { Header, Navbar, Settings, Layout }
+export { Header, Navbar, Settings, Layout, PlugForDesktop }
