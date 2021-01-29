fa97af1548c965694ddc9e5b67e61f3919fd47c2
sashka2131 <uhucbeucg@gmail.com@gmail.com>
2021-01-29 11:09:55 +0200

    add Settings component

--- src/app/components/Settings/Settings.template.js
+++ src/app/components/Settings/Settings.template.js
@@ -1,8 +1,42 @@
-const Settings = (props) => {
-  return <div>Settings</div>
+import { List, Paper, Typography } from '@material-ui/core'
+import { Category, Group, Cake, Assignment } from '@material-ui/icons'
+import { Container, Row, Col } from '@qonsoll/react-design'
+
+const PAGES = {
+  Users: <Category color="primary" fontSize="large" />,
+  Products: <Group color="primary" fontSize="large" />,
+  Categories: <Cake color="primary" fontSize="large" />,
+  Logs: <Assignment color="primary" fontSize="large" />
+}
+const PAGES_KEYS = Object.keys(PAGES)
+
+const Settings = () => {
+  return (
+    <List>
+      {PAGES_KEYS.map((page) => {
+        return (
+          <Container mb={3}>
+            <Row>
+              <Col>
+                <Paper>
+                  <Row>
+                    <Col cw="auto" v="center" p="2">
+                      {PAGES[page]}
+                    </Col>
+                    <Col cw="auto" v="center">
+                      <Typography variant="subtitle1">{page}</Typography>
+                    </Col>
+                  </Row>
+                </Paper>
+              </Col>
+            </Row>
+          </Container>
+        )
+      })}
+    </List>
+  )
 }
 
 Settings.propTypes = {}
-Settings.defaultProps = {}
 
 export default Settings
