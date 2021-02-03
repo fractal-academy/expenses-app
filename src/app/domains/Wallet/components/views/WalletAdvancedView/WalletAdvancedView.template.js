4db119ce3c29de5d6d254d1f1be62f90e90ae20c
olenashevchuk <olenashevchuk0@gmail.com>
2021-02-03 14:53:21 +0200

    fixed and added comment to walletAdvancedView

--- src/app/domains/Wallet/components/views/WalletAdvancedView/WalletAdvancedView.template.js
+++ src/app/domains/Wallet/components/views/WalletAdvancedView/WalletAdvancedView.template.js
@@ -1,59 +1,95 @@
 import { Row, Container, Col, Box } from '@qonsoll/react-design'
 import { Typography, IconButton, Paper } from '@material-ui/core'
-import { MemberSimpleView } from 'app/domains/Member/components/views/MemberSimpleView'
-import { useStyles } from './WalletAdvancedView.styles'
-import EditOutlinedIcon from '@material-ui/icons/EditOutlined'
-import PaymentIcon from '@material-ui/icons/Payment'
+import { MemberAdvancedView } from 'domains/Member/components/views/MemberAdvancedView'
+import { WalletCombined } from 'domains/Wallet/components/combined/WalletCombined'
 import { CurrencySimpleView } from 'domains/Currency/components/views'
+import { useStyles } from './WalletAdvancedView.styles'
+import { MoreHorizOutlined, Edit, Delete } from '@material-ui/icons'
+import { DropdownItem, Dropdown } from 'app/components/Lib/Dropdown'
+
+const DropdownList = (
+  <>
+    <WalletCombined title="Edit wallet" typeModalEdit>
+      <DropdownItem>
+        <Box mr={2}>
+          <Edit />
+        </Box>
+        Edit
+      </DropdownItem>
+    </WalletCombined>
+    <DropdownItem danger>
+      <Box mr={2}>
+        <Delete />
+      </Box>
+      Delete
+    </DropdownItem>
+  </>
+)
 
 const WalletAdvancedView = (props) => {
   const classes = useStyles()
 
-  const { nameWallet, balance, owner, currency } = props
+  const { nameWallet, balance, owner, currency, avatarUrl } = props
 
   return (
-    <Container mb={3}>
+    <Container>
       <Row>
         <Col>
           <Paper elevation={3} className={classes.styledPaper}>
-            <Row height="100%" p={2}>
-              <Col>
-                <Row h="between" v="center" mb={3}>
-                  <Col cw="auto">
-                    <Typography variant="h6">{nameWallet}</Typography>
-                  </Col>
-                  <Col cw="auto">
-                    <IconButton
-                      size="small"
-                      color="primary"
-                      aria-label="upload picture"
-                      component="span">
-                      <EditOutlinedIcon />
-                    </IconButton>
-                  </Col>
-                </Row>
-                <Row h="between" v="center">
-                  <Col cw="auto">
-                    <MemberSimpleView withName name={'Olena'} />
-                  </Col>
-                  <Col cw="auto">
-                    <Row>
-                      <Col>
-                        <Typography variant="caption">Balance</Typography>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                        <Box display="flex">
-                          <Typography variant="h6">{balance}</Typography>
-                          <CurrencySimpleView variant="h6" value={currency} />
-                        </Box>
-                      </Col>
-                    </Row>
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
+            <Container p={2}>
+              <Row>
+                <Col>
+                  <Row h="between" v="center" mb={4}>
+                    {/*there are name of wallet and drop down with edit and delete functions*/}
+                    <Col cw="auto">
+                      <Typography variant="h6">{nameWallet}</Typography>
+                    </Col>
+                    <Col cw="auto">
+                      <Dropdown overlay={DropdownList}>
+                        <IconButton
+                          size="small"
+                          color="primary"
+                          aria-label="upload picture"
+                          component="span">
+                          <MoreHorizOutlined />
+                        </IconButton>
+                      </Dropdown>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+              <Row v="center" noGutters>
+                <Col>
+                  <Row>
+                    {/*there is member`s info*/}
+                    <Col>
+                      <MemberAdvancedView
+                        horizontal
+                        role={'owner'}
+                        name={owner}
+                        avatarUrl={avatarUrl}
+                      />
+                    </Col>
+                  </Row>
+                </Col>
+                <Col cw="auto">
+                  {/*there is info about balance*/}
+                  <Row>
+                    <Col>
+                      <Typography variant="caption">Balance</Typography>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Box display="flex">
+                        <Typography variant="body1">{balance}</Typography>
+                        <CurrencySimpleView variant="body1" value={currency} />
+                      </Box>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Container>
           </Paper>
         </Col>
       </Row>
