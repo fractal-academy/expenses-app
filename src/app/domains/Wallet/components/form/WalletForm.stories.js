d6268f378a130de46ef6ebcc198fa9ee3c619d67
olenashevchuk <olenashevchuk0@gmail.com>
2021-02-03 15:08:26 +0200

    create walletForm

---
+++ src/app/domains/Wallet/components/form/WalletForm.stories.js
@@ -0,0 +1,31 @@
+import WalletForm from 'app/domains/Wallet/components/form/WalletForm.template'
+
+const metadata = {
+  title: 'domains/Wallet/components/form/WalletForm',
+  component: WalletForm,
+  argsType: {
+    onSubmit: { action: 'onSubmit' },
+    onSubmitFail: { action: 'onSubmitFail' },
+    show: {
+      control: {
+        type: 'select',
+        options: ['nameWallet', 'member', 'balance', 'currency']
+      }
+    }
+  }
+}
+export default metadata
+
+const Template = (args) => <WalletForm {...args} />
+
+export const WalletFormStory = Template.bind({})
+
+WalletFormStory.args = {
+  formData: {
+    nameWallet: 'NameWallet',
+    balance: '1000',
+    currency: 'USD',
+    member: 'Olena'
+  },
+  show: ['nameWallet', 'member', 'balance', 'currency']
+}
