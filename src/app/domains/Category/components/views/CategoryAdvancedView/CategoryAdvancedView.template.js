3d51fd81504617cbc8ad914f723ac985fde68bbd
olenashevchuk <olenashevchuk0@gmail.com>
2021-02-08 18:06:51 +0200

    new layout categoryAdvancedView

--- src/app/domains/Category/components/views/CategoryAdvancedView/CategoryAdvancedView.template.js
+++ src/app/domains/Category/components/views/CategoryAdvancedView/CategoryAdvancedView.template.js
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types'
 import { Row, Container, Col, Box } from '@qonsoll/react-design'
-import { Typography, IconButton, Paper } from '@material-ui/core'
+import { Typography, IconButton, Paper, Divider } from '@material-ui/core'
 import { MoreHorizOutlined, Edit, Delete } from '@material-ui/icons'
 import { CurrencySimpleView } from 'domains/Currency/components/views'
 import { ProgressBar } from 'app/components/Lib'
 import { useStyles } from './CategoryAdavncedView.style'
 import { CategoryCombined } from 'domains/Category/components/combined/CategoryCombined'
 import { DropdownItem, Dropdown } from 'app/components/Lib/Dropdown'
+import formatCurrency from 'format-currency'
 
 const DropdownList = (
   <div>
@@ -37,85 +38,100 @@ const CategoryAdvancedView = (props) => {
     budget,
     valueForProgressBar
   } = props
+
   const availableBalance = budget - spent
+
+  const formattedAvailableBalance = formatCurrency(availableBalance)
+
+  const formattedSpent = formatCurrency(spent)
+
   return (
-    <Container mb={3}>
-      <Row>
-        <Col>
-          <Paper
-            elevation={3}
-            className={classes.root}
-            style={{ overflow: 'hidden' }}>
-            {/* Row colorBox */}
-            <Row height="100%" pr="0.25rem" pl="0rem">
-              <Col cw="auto" px="0">
-                <Box width="0.35rem" height="100%" background={colorCategory} />
-              </Col>
-              <Col>
-                {/* Row with nameCategory and buttonEdit*/}
-                <Row h="between" v="center" mb={2}>
-                  <Col cw="auto">
-                    <Typography>{nameCategory}</Typography>
-                  </Col>
-                  <Col cw="auto">
-                    <Dropdown overlay={DropdownList}>
-                      <IconButton
-                        size="small"
-                        color="primary"
-                        aria-label="upload picture"
-                        component="span">
-                        <MoreHorizOutlined />
-                      </IconButton>
-                    </Dropdown>
-                  </Col>
-                </Row>
-                {/*Row with Available balance*/}
-                <Row h="between" v="center" mb={2}>
-                  <Col cw="auto">
-                    <Typography variant="body2">Available Balance</Typography>
-                  </Col>
-                  <Col cw="auto">
-                    <Box display="flex">
-                      <Typography variant="body2">
-                        {availableBalance}
+    <>
+      <Container mb={3} className={classes.paper}>
+        <Row>
+          <Col>
+            <Paper>
+              <Box className={classes.border} background={colorCategory} />
+              {/* Row colorBox */}
+              <Row height="100%" pr="0.25rem" pl="0rem">
+                <Col p={2}>
+                  {/* Row with nameCategory and buttonEdit*/}
+                  <Row h="between" v="center" mb={2}>
+                    <Col cw="auto">
+                      <Typography variant="h6" className={classes.bold}>
+                        {nameCategory}
                       </Typography>
-                      <CurrencySimpleView variant="body2" value={currency} />
-                    </Box>
-                  </Col>
-                </Row>
-                {/* Row with ProgressBar*/}
-                <Row mb={2}>
-                  <Col sw="12">
-                    <ProgressBar value={valueForProgressBar} />
-                  </Col>
-                </Row>
-                {/*Row with Spent and BudgetValue*/}
-                <Row h="between">
-                  <Col cw="auto">
-                    <Box display="flex">
-                      <Typography component={Box} pr={1} variant="body2">
-                        Spent
+                    </Col>
+                    <Col cw="auto">
+                      <Dropdown overlay={DropdownList}>
+                        <IconButton
+                          size="small"
+                          color="primary"
+                          aria-label="upload picture"
+                          component="span">
+                          <MoreHorizOutlined />
+                        </IconButton>
+                      </Dropdown>
+                    </Col>
+                  </Row>
+                  {/*Row with divider*/}
+                  <Row mb={3}>
+                    <Col cw={12}>
+                      <Divider />
+                    </Col>
+                  </Row>
+                  {/*Row with spent and left money*/}
+                  <Row h="between" mb={1}>
+                    <Col cw="auto" display="flex">
+                      <CurrencySimpleView
+                        variant="body1"
+                        value={currency}
+                        className={
+                          budget - spent > 50
+                            ? `${classes.blue}`
+                            : `${classes.red}`
+                        }
+                      />
+                      <Typography
+                        variant="body1"
+                        className={
+                          budget - spent > 50
+                            ? `${classes.blue}`
+                            : `${classes.red}`
+                        }>
+                        {formattedAvailableBalance}
                       </Typography>
-                      <Typography variant="body2">{spent}</Typography>
-                      <CurrencySimpleView variant="body2" value={currency} />
-                    </Box>
-                  </Col>
-                  <Col cw="auto">
-                    <Box display="flex">
-                      <Typography component={Box} pr={1} variant="body2">
-                        Budget
+                    </Col>
+
+                    <Col cw="auto" display="flex">
+                      <CurrencySimpleView
+                        color="textSecondary"
+                        variant="body1"
+                        value={currency}
+                        className={budget - spent < 50 && `${classes.red}`}
+                      />
+                      <Typography
+                        variant="body1"
+                        color="textSecondary"
+                        className={budget - spent < 50 && `${classes.red}`}>
+                        {formattedSpent}
                       </Typography>
-                      <Typography variant="body2">{budget}</Typography>
-                      <CurrencySimpleView variant="body2" value={currency} />
-                    </Box>
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </Paper>
-        </Col>
-      </Row>
-    </Container>
+                    </Col>
+                  </Row>
+
+                  {/* Row with ProgressBar*/}
+                  <Row mb={2}>
+                    <Col sw="12">
+                      <ProgressBar value={valueForProgressBar} />
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Paper>
+          </Col>
+        </Row>
+      </Container>
+    </>
   )
 }
 CategoryAdvancedView.propTypes = {
