d505d809d83b77778ae7b2d2f7ffce19d121f574
okrdima <okrdima@gmail.com>
2021-02-08 08:35:16 +0200

    fix statistic view

--- src/app/domains/Statistic/components/views/StatisticAdvancedView/StatisticAdvancedView.template.js
+++ src/app/domains/Statistic/components/views/StatisticAdvancedView/StatisticAdvancedView.template.js
@@ -1,78 +1,31 @@
 import Chart from 'react-apexcharts'
 import { useStatisticContext } from 'app/context/StatisticsContext'
-import 'moment/locale/uk'
 import moment from 'moment'
-import 'moment/locale/uk'
 import _ from 'lodash'
-const ukMoment = moment().locale('uk')
-
-const options = {
-  chart: {
-    id: 'basic-bar',
-    animations: {
-      speed: 200
-    }
-  },
-  legend: {
-    fontSize: 15,
-    markers: {
-      radius: 4,
-      width: 22
-    },
-    itemMargin: {
-      vertical: 4
-    }
-  },
-  tooltip: {
-    enabled: false
-  },
-  plotOptions: {
-    pie: {
-      donut: {
-        labels: {
-          show: true,
-          total: {
-            show: true,
-            fontWeight: 600,
-            color: '#373d3f'
-          }
-        }
-      }
-    }
-  },
-
-  dataLabels: {
-    formatter: function (val, opts) {
-      return opts.w.globals.series[opts.seriesIndex]
-    }
-  }
-}
-
-const series = [5030, 3000, 1000, 1400]
 
 const mockData = [
   {
     nameProduct: 'Day',
     category: 'Kitchen',
-    dateBuy: 1612648800,
+    dateBuy: 1612765683,
+    price: 1000
+  },
+  {
+    nameProduct: 'Day',
+    category: 'Office',
+    dateBuy: 1612765683,
     price: 10
   },
   {
     nameProduct: 'Day',
     category: 'Office',
-    dateBuy: 1612648800,
-    price: 10
-  },
-  {
-    nameProduct: 'Day',
-    category: 'Office',
-    dateBuy: 1612648800,
+    dateBuy: 1612765683,
     price: 10
   },
   {
     nameProduct: 'Week',
     category: 'Food',
-    dateBuy: 1612130400,
+    dateBuy: 1612938483,
     price: 100
   },
 
@@ -86,25 +39,25 @@ const mockData = [
     nameProduct: 'Year',
     category: 'Sport',
     dateBuy: 1609452000,
-    price: 10000
+    price: 1000
   },
   {
     nameProduct: 'Year',
     category: 'Sport',
     dateBuy: 1609452000,
-    price: 10000
+    price: 100
   },
   {
     nameProduct: 'Year',
     category: 'Sport',
     dateBuy: 1609452000,
-    price: 10000
+    price: 100
   },
   {
     nameProduct: 'Year',
     category: 'Sport',
     dateBuy: 1609452000,
-    price: 10000
+    price: 1000
   }
 ]
 const func = (range) => {
@@ -112,6 +65,7 @@ const func = (range) => {
   const rangeEnd = parseInt(moment(range.endDate).format('X'))
   const arrCategoryName = []
   const productsSum = []
+  const res = []
   // map for create Arr with name categories
   mockData.forEach((item) => {
     if (rangeStart <= item.dateBuy && item.dateBuy <= rangeEnd) {
@@ -120,9 +74,7 @@ const func = (range) => {
   })
   // return new Arr without duplicate name categories
   const resArrCategory = _.uniqWith(arrCategoryName, _.isEqual)
-
   resArrCategory.forEach((nameCategory) => {
-    const res = []
     mockData.forEach((item) => {
       if (
         item.category === nameCategory &&
@@ -142,9 +94,50 @@ const func = (range) => {
 const StatisticAdvancedView = (props) => {
   const { state, setState } = useStatisticContext()
 
-  const [arrCategoryName, productSum] = func(state.date)
-  options.labels = arrCategoryName
-  console.log(arrCategoryName, productSum)
+  const [resArrCategory, productSum] = func(state.date)
+
+  const options = {
+    chart: {
+      id: 'basic-bar',
+      animations: {
+        speed: 200
+      }
+    },
+    legend: {
+      fontSize: 15,
+      markers: {
+        radius: 4,
+        width: 22
+      },
+      itemMargin: {
+        vertical: 4
+      }
+    },
+    tooltip: {
+      enabled: true
+    },
+    plotOptions: {
+      pie: {
+        donut: {
+          labels: {
+            show: true,
+            total: {
+              show: true,
+              fontWeight: 600,
+              color: '#373d3f'
+            }
+          }
+        }
+      }
+    },
+    labels: resArrCategory,
+    dataLabels: {
+      formatter: function (val, opts) {
+        return opts.w.globals.series[opts.seriesIndex]
+      }
+    }
+  }
+
   return <Chart options={options} series={productSum} type="donut" />
 }
 
