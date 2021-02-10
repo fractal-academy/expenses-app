44063716a9581543c9708c61ddb93effbec06cb2
olenashevchuk <olenashevchuk0@gmail.com>
2021-02-10 17:38:29 +0200

    added delete wallet

--- src/app/domains/Wallet/components/views/WalletAdvancedView/WalletAdvancedView.template.js
+++ src/app/domains/Wallet/components/views/WalletAdvancedView/WalletAdvancedView.template.js
@@ -1,5 +1,6 @@
 import { Row, Container, Col, Box } from '@qonsoll/react-design'
-import { Typography, IconButton, Paper } from '@material-ui/core'
+import { Typography, IconButton, Paper, Snackbar } from '@material-ui/core'
+import { Alert } from '@material-ui/lab'
 import { MemberAdvancedView } from 'domains/Member/components/views/MemberAdvancedView'
 import { WalletCombined } from 'domains/Wallet/components/combined/WalletCombined'
 import { CurrencySimpleView } from 'domains/Currency/components/views'
@@ -8,7 +9,9 @@ import { useStyles } from './WalletAdvancedView.styles'
 import { MoreHorizOutlined, Edit, Delete } from '@material-ui/icons'
 import { DropdownItem, Dropdown } from 'app/components/Lib/Dropdown'
 import { getData } from 'app/services'
-import { useEffect, useState } from 'react'
+import { deleteData } from 'app/services/Firestore'
+import { useEffect, useState, Fragment } from 'react'
+import { COLLECTIONS } from 'app/constants'
 
 const WalletAdvancedView = (props) => {
   const classes = useStyles()
@@ -23,16 +26,30 @@ const WalletAdvancedView = (props) => {
   } = props
 
   const [memberData, setMemberData] = useState()
+  const [openSnackbarSuccess, setOpenSnackbarSuccess] = useState(false)
+  const [openSnackbarError, setOpenSnackbarError] = useState(false)
+
+  const handleClose = () => {
+    setOpenSnackbarSuccess(false)
+    setOpenSnackbarError(false)
+  }
 
   //useEffect needs refactor
   useEffect(
     () =>
       idMember &&
-      getData('users', idMember).then((item) => setMemberData(item)),
+      getData(COLLECTIONS.USERS, idMember).then((item) => setMemberData(item)),
     []
   )
 
-  const deleteWallet = async () => {}
+  const deleteWallet = async () => {
+    try {
+      await deleteData(COLLECTIONS.WALLETS, idWallet)
+      setOpenSnackbarSuccess(true)
+    } catch (error) {
+      setOpenSnackbarError(true)
+    }
+  }
   const DropdownList = (
     <>
       <WalletCombined
@@ -60,91 +77,112 @@ const WalletAdvancedView = (props) => {
     </>
   )
   return (
-    <Container mb={3}>
-      <Row>
-        <Col>
-          <Paper className={classes.styledPaper}>
-            <Container p={2}>
-              <Row>
-                <Col>
-                  <Row h="between" v="center" mb={4}>
-                    {/*there are name of wallet and drop down with edit and delete functions*/}
+    <Fragment>
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={openSnackbarSuccess}
+        autoHideDuration={6000}
+        onClose={handleClose}>
+        <Alert variant="filled" severity="success">
+          Wallet is deleted!
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={openSnackbarError}
+        autoHideDuration={6000}
+        onClose={handleClose}>
+        <Alert variant="filled" severity="error">
+          Error
+        </Alert>
+      </Snackbar>
 
-                    <Col cw="auto">
-                      <Typography variant="body1">{nameWallet}</Typography>
-                    </Col>
-                    <Col cw="auto">
-                      <Dropdown overlay={DropdownList}>
-                        <IconButton
-                          size="small"
-                          color="primary"
-                          aria-label="upload picture"
-                          component="span">
-                          <MoreHorizOutlined />
-                        </IconButton>
-                      </Dropdown>
-                    </Col>
-                  </Row>
-                </Col>
-              </Row>
-              <Row v="center" noGutters>
-                <Col>
-                  <Row>
-                    {/*there is member`s info*/}
+      <Container mb={3}>
+        <Row>
+          <Col>
+            <Paper className={classes.styledPaper}>
+              <Container p={2}>
+                <Row>
+                  <Col>
+                    <Row h="between" v="center" mb={4}>
+                      {/*there are name of wallet and drop down with edit and delete functions*/}
 
-                    <Col>
-                      {privateWallet ? (
-                        <MemberAdvancedView
-                          horizontal
-                          name={memberData && memberData.firstName}
-                          surname={memberData && memberData.surname}
-                          avatarUrl={memberData && memberData.avatarURL}
-                          role={'owner'}
-                        />
-                      ) : (
-                        <Container>
-                          <Row v="center">
-                            <Col cw="auto">
-                              <Avatar
-                                src="https://firebasestorage.googleapis.com/v0/b/expenses-app-development-9ba1c.appspot.com/o/logo-white(sense)-color(teq).jpg?alt=media&token=757d0b30-a6b6-4637-be0c-cc8efbc3f69f"
-                                alt="Senseteq"
-                              />
-                            </Col>
-                            <Col cw="auto">
-                              <Typography>Senseteq</Typography>
-                            </Col>
-                          </Row>
-                        </Container>
-                      )}
-                    </Col>
-                  </Row>
-                </Col>
-                <Col cw="auto">
-                  {/*there is info about balance*/}
+                      <Col cw="auto">
+                        <Typography variant="body1">{nameWallet}</Typography>
+                      </Col>
+                      <Col cw="auto">
+                        <Dropdown overlay={DropdownList}>
+                          <IconButton
+                            size="small"
+                            color="primary"
+                            aria-label="upload picture"
+                            component="span">
+                            <MoreHorizOutlined />
+                          </IconButton>
+                        </Dropdown>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+                <Row v="center" noGutters>
+                  <Col>
+                    <Row>
+                      {/*there is member`s info*/}
 
-                  <Row>
-                    <Col>
-                      <Typography variant="caption">Balance</Typography>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col>
-                      <Box display="flex">
-                        <Typography variant="body1">{balance}</Typography>
-                        <CurrencySimpleView
-                          variant="body1"
-                          value={idCurrency}
-                        />
-                      </Box>
-                    </Col>
-                  </Row>
-                </Col>
-              </Row>
-            </Container>
-          </Paper>
-        </Col>
-      </Row>
-    </Container>
+                      <Col>
+                        {privateWallet ? (
+                          <MemberAdvancedView
+                            horizontal
+                            name={memberData && memberData.firstName}
+                            surname={memberData && memberData.surname}
+                            avatarUrl={memberData && memberData.avatarURL}
+                            role={'owner'}
+                          />
+                        ) : (
+                          <Container>
+                            <Row v="center">
+                              <Col cw="auto">
+                                <Avatar
+                                  src="https://firebasestorage.googleapis.com/v0/b/expenses-app-development-9ba1c.appspot.com/o/logo-white(sense)-color(teq).jpg?alt=media&token=757d0b30-a6b6-4637-be0c-cc8efbc3f69f"
+                                  alt="Senseteq"
+                                />
+                              </Col>
+                              <Col cw="auto">
+                                <Typography>Senseteq</Typography>
+                              </Col>
+                            </Row>
+                          </Container>
+                        )}
+                      </Col>
+                    </Row>
+                  </Col>
+                  <Col cw="auto">
+                    {/*there is info about balance*/}
+
+                    <Row>
+                      <Col>
+                        <Typography variant="caption">Balance</Typography>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <Box display="flex">
+                          <Typography variant="body1">{balance}</Typography>
+                          <CurrencySimpleView
+                            variant="body1"
+                            value={idCurrency}
+                          />
+                        </Box>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+              </Container>
+            </Paper>
+          </Col>
+        </Row>
+      </Container>
+    </Fragment>
   )
 }
 
