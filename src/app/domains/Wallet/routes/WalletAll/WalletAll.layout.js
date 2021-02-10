de2e7f38970b950abde03e7b86578beccbfa49c8
olenashevchuk <olenashevchuk0@gmail.com>
2021-02-10 11:38:55 +0200

    fixed layout WalletAll (delete wrapper)

--- src/app/domains/Wallet/routes/WalletAll/WalletAll.layout.js
+++ src/app/domains/Wallet/routes/WalletAll/WalletAll.layout.js
@@ -1,16 +1,16 @@
 import { WalletList } from 'app/domains/Wallet/components/list'
 import { WalletCombined } from 'app/domains/Wallet/components/combined'
-import { Container } from '@qonsoll/react-design'
+import { Fragment } from 'react'
+
 const WalletAll = (props) => {
   return (
-    <Container>
+    <Fragment>
       <WalletList />
       <WalletCombined title={'Create new wallet'} />
-    </Container>
+    </Fragment>
   )
 }
 
 WalletAll.propTypes = {}
-WalletAll.defaultProps = {}
 
 export default WalletAll
