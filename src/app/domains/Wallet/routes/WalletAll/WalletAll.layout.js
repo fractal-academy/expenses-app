f2babba6f8a1b3312e91186722af292277900d14
olenashevchuk <olenashevchuk0@gmail.com>
2021-02-10 20:46:27 +0200

    deleted Fragment

--- src/app/domains/Wallet/routes/WalletAll/WalletAll.layout.js
+++ src/app/domains/Wallet/routes/WalletAll/WalletAll.layout.js
@@ -1,6 +1,5 @@
 import { WalletList } from 'app/domains/Wallet/components/list'
 import { WalletCombined } from 'app/domains/Wallet/components/combined'
-import { Fragment } from 'react'
 
 const WalletAll = (props) => {
   return (
