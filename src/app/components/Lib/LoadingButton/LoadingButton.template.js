eda8e74af2c113c766fd493664517b00e2f0b014
Maxim <maxim.makarov14@gmail.com>
2021-02-11 08:28:53 +0200

    Added icon uploader support to LoadingButton

--- src/app/components/Lib/LoadingButton/LoadingButton.template.js
+++ src/app/components/Lib/LoadingButton/LoadingButton.template.js
@@ -1,23 +1,32 @@
-import { useRef, useEffect } from 'react'
+import { useRef } from 'react'
 import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
 const LoadingButton = (props) => {
-  const { loading, children, ...rest } = props
+  const { loading, startIcon, children, ...rest } = props
   const buttonRef = useRef(null)
 
   return (
     <Button
       ref={buttonRef}
       {...rest}
-      color={loading ? 'default' : rest.color}
+      color={loading && rest.variant ? 'default' : rest.color}
       style={
         buttonRef.current && {
           width: buttonRef.current.clientWidth,
           height: buttonRef.current.clientHeight
         }
+      }
+      startIcon={
+        (startIcon && loading && (
+          <CircularProgress
+            color="inherit"
+            size={buttonRef.current.clientHeight / 2}
+          />
+        )) ||
+        startIcon
       }>
-      {loading ? (
+      {loading && !startIcon ? (
         <CircularProgress
           color="inherit"
           size={buttonRef.current.clientHeight / 1.5}
