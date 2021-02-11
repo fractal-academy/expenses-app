9ca2ecc1907106badf7a574bf88bb4f45c55597d
Rostyslav <rostyslavmarchuk440@gmail.com>
2021-02-11 12:35:51 +0200

    Refactoring

--- src/app/domains/Notification/components/list/NotificationList/NotificationList.template.js
+++ src/app/domains/Notification/components/list/NotificationList/NotificationList.template.js
@@ -1,11 +1,11 @@
-import { NotificationAdvancedView } from '../../views/NotificationAdvancedView'
+import { NotificationAdvancedView } from 'domains/Notification/components/views'
 import { useState, useEffect } from 'react'
-import { firestore, getData } from 'app/services/Firestore'
+import { getCollectionRef, getData } from 'app/services/Firestore'
 import { useCollection } from 'react-firebase-hooks/firestore'
 import { useSession } from 'app/context/SessionContext/hooks'
+import { COLLECTIONS } from 'app/constants'
 import md5 from 'md5'
 
-//TODO delete mock data
 const NotificationList = (props) => {
   // STATE
   const [data, setData] = useState([])
@@ -13,14 +13,16 @@ const NotificationList = (props) => {
   // CUSTOM HOOKS
   const session = useSession()
   const [value, loading, error] = useCollection(
-    firestore
-      .collection('notifications')
-      .where('userId', '==', md5(session.email))
+    getCollectionRef(COLLECTIONS.NOTIFICATIONS).where(
+      'userId',
+      '==',
+      md5(session.email)
+    )
   )
 
   // USE EFFECTS
   useEffect(() => {
-    getData('users', md5(session.email)).then((result) => {
+    getData(COLLECTIONS.USERS, md5(session.email)).then((result) => {
       const notifications =
         value &&
         value.docs.map((item) => {
--- src/app/domains/Notification/components/views/NotificationSimpleView/NotificationSimpleView.template.js
+++ src/app/domains/Notification/components/views/NotificationSimpleView/NotificationSimpleView.template.js
@@ -1,11 +1,11 @@
 import { Notifications } from '@material-ui/icons'
 import { IconButton, Badge } from '@material-ui/core'
 import { useEffect, useState } from 'react'
-import { firestore } from 'app/services/Firestore'
+import { getCollectionRef } from 'app/services/Firestore'
 import { useCollection } from 'react-firebase-hooks/firestore'
 import { useSession } from 'app/context/SessionContext/hooks'
 import { useHistory } from 'react-router-dom'
-import { ROUTES_PATHS } from 'app/constants'
+import { ROUTES_PATHS, COLLECTIONS } from 'app/constants'
 import md5 from 'md5'
 
 const NotificationSimpleView = (props) => {
@@ -16,9 +16,11 @@ const NotificationSimpleView = (props) => {
   const session = useSession()
   const history = useHistory()
   const [value, loading, error] = useCollection(
-    firestore
-      .collection('notifications')
-      .where('userId', '==', md5(session.email))
+    getCollectionRef(COLLECTIONS.NOTIFICATIONS).where(
+      'userId',
+      '==',
+      md5(session.email)
+    )
   )
 
   // USE EFFECTS
