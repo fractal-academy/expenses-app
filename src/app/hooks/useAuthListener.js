3b55e4e5fd166f85c7161724016415e84435b074
5P4C3GR33D <cyber.cuttlefish@gmail.com>
2021-02-12 15:17:26 +0200

    fixed session bug

--- src/app/hooks/useAuthListener.js
+++ src/app/hooks/useAuthListener.js
@@ -87,7 +87,7 @@ const useAuthListener = () => {
     //if user loaded -> fetch his data
     !!user && !userLoading && fetchUser()
     user && session && setLoading(false)
-  }, [user, userLoading, session])
+  }, [user, userLoading])
 
   useEffect(() => {
     const unsubscribe =
@@ -105,7 +105,7 @@ const useAuthListener = () => {
           dispatch({ type: types.LOGIN_USER, payload: data })
           console.log('in')
         })
-    return () => unsubscribe()
+    return () => unsubscribe && unsubscribe()
   }, [])
 
   useEffect(() => {
