a449afe8a53e461e8bcc0cd72f50a70302e0b8c0
Maxim <maxim.makarov14@gmail.com>
2021-02-12 13:18:26 +0200

    Implement member editing

--- src/app/components/Lib/LoadingButton/LoadingButton.template.js
+++ src/app/components/Lib/LoadingButton/LoadingButton.template.js
@@ -1,36 +1,32 @@
-import { useRef } from 'react'
+import { useCallback, useState } from 'react'
 import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
+//todo need width fix
 const LoadingButton = (props) => {
   const { loading, startIcon, children, ...rest } = props
-  const buttonRef = useRef(null)
-
+  const [size, setSize] = useState({ width: 0, height: 0 })
+  const buttonRef = useCallback((node) => {
+    if (node) {
+      setSize({
+        width: node.clientWidth,
+        height: node.clientHeight
+      })
+    }
+  }, [])
   return (
     <Button
       ref={buttonRef}
       {...rest}
       color={loading && rest.variant ? 'default' : rest.color}
-      style={
-        buttonRef.current && {
-          width: buttonRef.current.clientWidth,
-          height: buttonRef.current.clientHeight
-        }
-      }
       startIcon={
         (startIcon && loading && (
-          <CircularProgress
-            color="inherit"
-            size={buttonRef.current.clientHeight / 2}
-          />
+          <CircularProgress color="inherit" size={size.height / 2} />
         )) ||
         startIcon
       }>
-      {loading && !startIcon ? (
-        <CircularProgress
-          color="inherit"
-          size={buttonRef.current.clientHeight / 1.5}
-        />
+      {!startIcon && loading ? (
+        <CircularProgress color="inherit" size={size.height / 1.5} />
       ) : (
         children
       )}
--- src/app/domains/Member/routes/MemberEdit/MemberEdit.layout.js
+++ src/app/domains/Member/routes/MemberEdit/MemberEdit.layout.js
@@ -1,39 +1,62 @@
 import { useState, useEffect } from 'react'
-import { useHistory, useLocation } from 'react-router-dom'
+import { useHistory, useLocation, useParams } from 'react-router-dom'
 import { useSession } from 'app/context/SessionContext'
 import { MemberAdvancedForm } from 'domains/Member/components/forms'
 import { useForm } from 'react-hook-form'
 import { deleteURL } from 'app/services/Storage'
-import { setData } from 'app/services/Firestore'
+import { getData, setData } from 'app/services/Firestore'
 import { COLLECTIONS } from 'app/constants'
+import { Spinner } from 'app/components/Lib'
 
 const MemberEdit = (props) => {
-  const [hide, setHide] = useState([])
+  const [hide, setHide] = useState()
   const [loading, setLoading] = useState(false)
+  const [pageLoading, setPageLoading] = useState(true)
+  const [userData, setUserData] = useState({})
 
   const location = useLocation()
+  const { id } = useParams()
   let history = useHistory()
   const user = useSession()
-  const form = useForm({ defaultValues: user })
+  const form = useForm()
 
   useEffect(() => {
     if (location.pathname.includes(user.id)) {
       setHide(['role'])
     }
+    return () => setLoading(false)
   }, [])
 
+  useEffect(() => {
+    setPageLoading(true)
+
+    if (id === user.id) {
+      setUserData(user)
+    } else {
+      const fetchUser = async () => {
+        const res = await getData(COLLECTIONS.USERS, id)
+        setUserData({ ...res, id })
+      }
+      fetchUser()
+    }
+  }, [id, user])
+
+  useEffect(() => {
+    if (Object.keys(userData).length) {
+      form.reset(userData)
+      setPageLoading(false)
+    }
+  }, [userData])
+
   const onSubmit = async (data) => {
+    setLoading(true)
     try {
-      setLoading(true)
-      // if (user.id !== md5(data.email)) {
-      //   await deleteData(COLLECTIONS.USERS, user.id)
-      // }
-      await setData(COLLECTIONS.USERS, user.id, data)
+      await setData(COLLECTIONS.USERS, userData.id, data)
+
       history.goBack()
     } catch (e) {
       console.log(e)
     }
-    setLoading(false)
   }
   const onCancel = async (data) => {
     const { avatarURL } = data
@@ -45,6 +68,11 @@ const MemberEdit = (props) => {
       console.log(e)
     }
   }
+
+  if (pageLoading) {
+    return <Spinner />
+  }
+
   return (
     <MemberAdvancedForm
       onSubmit={onSubmit}
@@ -52,6 +80,7 @@ const MemberEdit = (props) => {
       hide={hide}
       onCancel={onCancel}
       loading={loading}
+      profile
     />
   )
 }
