7aec66d2ed8324419855f096213a6d7efb905c13
Rostyslav <rostyslavmarchuk440@gmail.com>
2021-02-16 18:01:37 +0200

    reminder function to triger notifications for regular products

--- functions/functions/reminder.js
+++ functions/functions/reminder.js
@@ -1,20 +1,30 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const moment = require('moment')
 
 exports.reminder = functions.pubsub
-  .schedule('1 0 * * *')
+  .schedule('0 5 * * *')
   .timeZone('Europe/Kiev')
   .onRun(async (context) => {
-    const products = await admin.firestore().collection('regularProducts').get()
-    const remindedProducts = products.docs
-      .map((item) => {
-        return {
-          name: item.data().name,
-          date: item.data().remind,
-          userId: [item.data().assign] | []
-        }
-      })
-      .filter((item) => item.date.toDate().getDate() === new Date().getDate())
+    const products = await admin
+      .firestore()
+      .collection('regularProducts')
+      .where('remind', '!=', 'null')
+      .get()
+    const remindedProducts = await products.docs.map((item) => {
+      return {
+        name: item.data().name,
+        date: item.data().remind,
+        userId: item.data().assign
+      }
+    })
+
+    const filteredProducts = remindedProducts.filter(
+      (item) =>
+        moment(item.date).date() ===
+        moment(admin.firestore.Timestamp.now()).date()
+    )
+
     const adminsInfo = await admin
       .firestore()
       .collection('users')
@@ -22,15 +32,15 @@ exports.reminder = functions.pubsub
       .get()
     const adminsIds = adminsInfo.docs.map((item) => item.id)
 
-    remindedProducts &&
-      remindedProducts.forEach((item) => {
+    filteredProducts &&
+      filteredProducts.forEach((item) => {
         admin
           .firestore()
           .collection('notifications')
           .add({
-            date: item.date,
+            date: admin.firestore.Timestamp.now(),
             text: `You should buy ${item.name} today!`,
-            userId: item.userId | adminsIds
+            userId: item.userId ? [item.userId] : adminsIds
           })
       })
   })
--- src/app/domains/Cart/routes/CartAll/CartAll.layout.js
+++ src/app/domains/Cart/routes/CartAll/CartAll.layout.js
@@ -1,7 +1,26 @@
 import { COLLECTIONS } from 'app/constants'
 import { CartTable } from 'domains/Cart/components/table'
 import { ProductCombinedForm } from 'app/domains/Product/components/combined/ProductCombinedForm'
+import { firebase } from 'app/services'
 const CartAll = (props) => {
+  const tmp = async () => {
+    const products = await firebase
+      .firestore()
+      .collection('regularProducts')
+      .get()
+    const remindedProducts = products.docs
+      .map((item) => {
+        return {
+          name: item.data().name,
+          date: item.data().remind,
+          userId: [item.data().assign] || []
+        }
+      })
+      .filter((item) => item.date.toDate().getDate() === new Date().getDate())
+    console.log(remindedProducts)
+  }
+
+  tmp()
   return (
     <>
       <CartTable />
