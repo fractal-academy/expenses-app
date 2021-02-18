b17bc7a0596f037bff77249cd16c989d7aea3ef5
okrdima <okrdima@gmail.com>
2021-02-18 09:00:24 +0200

    fix noData in Statistic Wallet

--- src/app/domains/Statistic/components/CollapseWallet/CollapseWallet.template.js
+++ src/app/domains/Statistic/components/CollapseWallet/CollapseWallet.template.js
@@ -12,7 +12,7 @@ const CollapseWallet = (props) => {
   const { state } = useStatisticContext()
   const dataList = filterDataForStatisticsWallet(state.date, dataFromDB)
   const totalPublicWallet = totalSpentPublicWallet(dataFromDB)
-  return dataList.length > 0 ? (
+  return (
     <>
       <List component="nav" aria-labelledby="nested-list-subheader">
         {dataList.map((item) => (
@@ -23,26 +23,22 @@ const CollapseWallet = (props) => {
             memberWallet={item.wallets}
           />
         ))}
-        <Row>
-          <Col />
-          <Col cw="auto">
-            <Box display="flex">
-              <ListItemText primary="Public wallets: &nbsp;" />
-              <ListItemText primary={totalPublicWallet} />
-              <ListItemText>
-                <CurrencySimpleView />
-              </ListItemText>
-            </Box>
-          </Col>
-        </Row>
+        {!!dataList.length > 0 && (
+          <Row>
+            <Col />
+            <Col cw="auto">
+              <Box display="flex">
+                <ListItemText primary="Public wallets: &nbsp;" />
+                <ListItemText primary={totalPublicWallet} />
+                <ListItemText>
+                  <CurrencySimpleView />
+                </ListItemText>
+              </Box>
+            </Col>
+          </Row>
+        )}
       </List>
     </>
-  ) : (
-    <img
-      src="/noData.svg"
-      alt="no data"
-      style={{ width: '100%', height: '100px' }}
-    />
   )
 }
 CollapseWallet.propTypes = {
