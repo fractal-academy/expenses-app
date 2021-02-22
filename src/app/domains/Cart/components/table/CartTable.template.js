151c68d9e7483538d47b1f6798a7d12f0363bdb6
okrdima <39271906+okrdima@users.noreply.github.com>
2021-02-22 18:03:11 +0200

    Update CartTable.template.js

--- src/app/domains/Cart/components/table/CartTable.template.js
+++ src/app/domains/Cart/components/table/CartTable.template.js
@@ -134,7 +134,7 @@ const CartTable = (props) => {
         })
         /*
         delete current product from collection card */
-        await deleteData(COLLECTIONS.CART, item).then()
+        await deleteData(COLLECTIONS.CART, item)
         /*
         calculate sum for product*/
         sum = sum + +product.price
