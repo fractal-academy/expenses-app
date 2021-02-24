e42368d7b1b101b11a51f3448b1acea9b6d26e63
okrdima <okrdima@gmail.com>
2021-02-24 11:04:34 +0200

    optimization determineDateBuy

--- src/app/domains/Cart/routes/CartEdit/determineDateBuy.js
+++ src/app/domains/Cart/routes/CartEdit/determineDateBuy.js
@@ -2,7 +2,6 @@ import { getTimestamp } from 'app/services/Firestore'
 import { types } from 'app/context/MessageContext'
 
 const determineDateBuy = (data, messageDispatch) => {
-  let dateBuy = 0
   const dateInPicker = getTimestamp().fromDate(new Date(data.dateBuy))
   const dateNow = getTimestamp().fromDate(new Date())
   if (data.dateBuy && dateInPicker < dateNow) {
@@ -10,22 +9,19 @@ const determineDateBuy = (data, messageDispatch) => {
       type: types.OPEN_SUCCESS_MESSAGE,
       payload: 'Product was edited successfully'
     })
-    dateBuy = dateInPicker
-    return dateBuy
+    return dateInPicker
   } else if (data.dateBuy && dateInPicker > dateNow) {
     messageDispatch({
       type: types.OPEN_WARNING_MESSAGE,
       payload: 'Date of purchase was changed to now'
     })
-    dateBuy = dateNow
-    return dateBuy
+    return dateNow
   } else if (!data.dateBuy) {
     messageDispatch({
       type: types.OPEN_SUCCESS_MESSAGE,
       payload: 'Product was edited successfully'
     })
-    dateBuy = dateNow
-    return dateBuy
+    return dateNow
   }
 }
 export default determineDateBuy
