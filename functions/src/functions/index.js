ede9dd8ca596336c0a52206f8a114bc7768aa768
Maxim <maxim.makarov14@gmail.com>
2021-02-24 21:18:21 +0200

    Refactored deleteUser function to TS

--- functions/src/functions/index.js
+++ functions/src/functions/index.js
@@ -1,5 +1,5 @@
 import { sendMail } from './sendInvitationEmail'
-import { deleteUser } from './deleteUser'
+import deleteUser from './deleteUser'
 import * as reminder from './reminders'
 
 export { sendMail, deleteUser, reminder }
