9b1a911ec60778c970c2b89a36aa31ddf112cc9f
aleksandrgolyk <aleksandr.golyk@gmail.com>
2021-02-25 17:05:06 +0200

    deleted StatisticsAll.style.js

--- src/app/domains/Statistic/routes/StatisticAll/StatisticAll.style.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from '@material-ui/core/styles'
-import MuiCustomTheme from 'app/config/qonsollTheme/MuiCustomTheme'
-
-export const useStyles = makeStyles((theme) => ({
-  switcher: {
-    transitionDuration: '2s',
-    transitionTimingFunction: 'steps(4, end)'
-  }
-}))
