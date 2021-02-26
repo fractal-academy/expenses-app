9a51a5b275b8faef6fd5beb0d0be456daf70169f
Maxim <maxim.makarov14@gmail.com>
2021-02-26 13:45:34 +0200

    Changed component version

--- src/modules/qr-module/route/QRAdd/QRAdd.layout.js
+++ src/modules/qr-module/route/QRAdd/QRAdd.layout.js
@@ -4,7 +4,7 @@ import { COLLECTIONS, ROUTES_PATHS } from 'app/constants'
 /**
  * @info QrAdd (15 Feb 2021) // CREATION DATE
  *
- * @since 16 Feb 2021 ( v.0.0.2 ) // LAST-EDIT DATE
+ * @since 25 Feb 2021 ( v.0.0.3 ) // LAST-EDIT DATE
  *
  * @return {ReactComponent}
  */
